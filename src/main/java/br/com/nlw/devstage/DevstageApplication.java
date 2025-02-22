fc54102dce61c9baf1ef79cb664d1b1e9e28d764
Lucas Guima <lucasguima.felipe@gmail.com>
2025-02-22 18:14:49 -0300

    build(spring-boot): initial setup for spring

---
+++ src/main/java/br/com/nlw/devstage/DevstageApplication.java
@@ -0,0 +1,13 @@
+package br.com.nlw.devstage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevstageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevstageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/nlw/devstage/DevstageApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.nlw.devstage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevstageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
