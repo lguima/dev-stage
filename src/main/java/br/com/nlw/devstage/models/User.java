50a6f5a9b64eaf03ec3c6a3dca536c4afe99edb6
Lucas Guima <lucasguima.felipe@gmail.com>
2025-02-23 13:15:42 -0300

    feat(models): create User model

---
+++ src/main/java/br/com/nlw/devstage/models/User.java
@@ -0,0 +1,42 @@
+package br.com.nlw.devstage.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users", schema = "app")
+public class User {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "user_id")
+  private Integer id;
+
+  @Column(name = "user_name", nullable = false)
+  private String name;
+
+  @Column(name = "user_email", nullable = false, unique = true)
+  private String email;
+
+  public Integer getId() {
+    return id;
+  }
+
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+}
