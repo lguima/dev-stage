e872aae2e0c9d67a56757374808763b3f50ae12b
Lucas Guima <lucasguima.felipe@gmail.com>
2025-02-23 16:16:31 -0300

    feat(services/event): create methods for subscribers ranking

--- src/main/java/br/com/nlw/devstage/services/EventService.java
+++ src/main/java/br/com/nlw/devstage/services/EventService.java
@@ -1,7 +1,12 @@
 package br.com.nlw.devstage.services;
 
+import br.com.nlw.devstage.dto.SubscriptionRankingItem;
+import br.com.nlw.devstage.dto.SubscritpionRankingByUser;
+import br.com.nlw.devstage.exceptions.EventNotFoundException;
+import br.com.nlw.devstage.exceptions.InvalidIndicationException;
 import br.com.nlw.devstage.models.Event;
 import br.com.nlw.devstage.repositories.EventRepository;
+import br.com.nlw.devstage.repositories.SubscriptionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +18,9 @@ import java.util.regex.Pattern;
 public class EventService {
   @Autowired
   private EventRepository eventRepository;
+
+  @Autowired
+  private SubscriptionRepository subscriptionRepository;
 
   public Event addEvent(Event event) {
     event.setPrettyName(
@@ -32,5 +40,32 @@ public class EventService {
 
   public Event getEventByPrettyName(String prettyName) {
     return eventRepository.findByPrettyName(prettyName);
+  }
+
+  public List<SubscriptionRankingItem> getSubscribersRanking(String eventName) {
+    Event event = eventRepository.findByPrettyName(eventName);
+
+    if (event == null) {
+      throw new EventNotFoundException(eventName);
+    }
+
+    return subscriptionRepository.generateRankingByEvent(event.getEventId());
+  }
+
+  public SubscritpionRankingByUser getSubscribersRankingByUser(String eventName, Integer userId) {
+    List<SubscriptionRankingItem> subscriptionRankingItems = this.getSubscribersRanking(eventName);
+
+    SubscriptionRankingItem userRanking = subscriptionRankingItems.stream()
+      .filter(i -> i.userId().equals(userId))
+      .findFirst()
+      .orElse(null);
+
+    if (userRanking == null) {
+      throw new InvalidIndicationException();
+    }
+
+    Integer position = subscriptionRankingItems.indexOf(userRanking) + 1;
+
+    return new SubscritpionRankingByUser(userRanking, position);
   }
 }
