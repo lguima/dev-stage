a035902b5c4fe8a2f7150fb044ad01a6536d7fd9
Lucas Guima <lucasguima.felipe@gmail.com>
2025-02-23 13:16:27 -0300

    feat(repositories): create Subscription repository

---
+++ src/main/java/br/com/nlw/devstage/repositories/SubscriptionRepository.java
@@ -0,0 +1,10 @@
+package br.com.nlw.devstage.repositories;
+
+import br.com.nlw.devstage.models.Event;
+import br.com.nlw.devstage.models.Subscription;
+import br.com.nlw.devstage.models.User;
+import org.springframework.data.repository.CrudRepository;
+
+public interface SubscriptionRepository extends CrudRepository<Subscription, Integer> {
+  public Subscription findByEventAndSubscriber(Event event, User subscriber);
+}
